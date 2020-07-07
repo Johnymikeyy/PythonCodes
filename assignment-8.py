2b4a896c02bc59aaab4d3beb3719b23c56cbca5d
hakan <hakandikicia@gmail.com>
2020-07-07 11:50:39 +0300

    Finds out the most frequent number

--- assignment-8.py
+++ assignment-8.py
@@ -1,3 +1,4 @@
-import os
-print("current directory")
-print(os.getcwd())
+#Finds out the most frequent number in the given list.
+numbers = [1, 3, 7, 4, 3, 0, 3, 6, 3]
+frequent_num = max(numbers, key = numbers.count)
+print(f"The most frequent number is {frequent_num} and it was {numbers.count(frequent_num)} times repeated")
