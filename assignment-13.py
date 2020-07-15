2d9440fd640a69d93ca5cdffcc82f797f779f5b7
hakan <hakandikicia@gmail.com>
2020-07-15 12:01:19 +0300

    assignment-13 is added. Avarage of student's score

---
+++ assignment-13.py
@@ -0,0 +1,11 @@
+#Avarages of Student's Scores
+
+scores =  {"Mary" : 85, "Susan": 75, "Barry" : 65, "Alexis" : 88, "Jane" : 45, "Tina" : 100, \
+     "Tom" : 90, "Tim": 60}
+avg = 0
+count = 0
+for i, j in scores.items():
+    avg += j
+    count += 1
+
+print(f"{avg/count} is avarages of student's score")
